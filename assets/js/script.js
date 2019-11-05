bbc0cf8814fd89853a0172315ed441a274f7ecc3
francois80 <glavieux.francois@orange.fr>
2019-11-05 10:46:51 +0100

    ajour exercice 2 jquery partie 2

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+$(function(){ //ne charge le code js que lorsque la page est chargée
+  $('#image').dblclick(function(){
+    //$('#image').css('width','500px');
+    $('#image').width('500px');
+  });
+});
